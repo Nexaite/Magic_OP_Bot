dbc1c92d5cb7758069f9470dbdd331e88c5ff9c8
Jordan <39842192+Nexaite@users.noreply.github.com>
2022-03-18 17:51:41 -0400

    Initial commit

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const dotenv = require('dotenv')
 const discord = require('discord.js')
 
 //Create a new Discord client
-
 const client = new discord.Client()
 
+//When client is ready, logs 'Bot is ready!'
 client.on('ready', (err) => {
     if(err) console.log(err)
-    console.log('Bot is online!')
+    console.log('Bot is ready!')
+})
+
+//Make the client login and start
+client.login(process.env.token, () => {
+    console.log('Logged In!')
 })
